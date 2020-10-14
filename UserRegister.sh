ee4aca7f2319aa0beaed0f5fd3a2b586629c1adc
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-14 21:32:45 +0100

    Modified:password validation exactly 1 special character

--- UserRegister.sh
+++ UserRegister.sh
@@ -33,7 +33,7 @@ else
         echo "$Mobile Invalid"
 fi
 read -p "Enter Password:" pass
-pat='^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}$'
+pat='^(?=.*[A-Z])(?=.*\d)(?=.*[!@%^&$*])[a-zA-Z0-9!@%^$&*]{8,}$'
 if [[ $pass =~ $pat ]]
 then
         echo "$pass valid"
