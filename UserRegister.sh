8b932414bff6610996c1f7f23340108acc17a4f3
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-14 11:32:07 +0100

    Added:First name validation

--- UserRegister.sh
+++ UserRegister.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "USER REGISTRATION"
+read -p "Enter First Name:" Fname
+pat='^([A-Z]{1}[A-za-z]{2,})+$'
+if [[ $Fname =~ $pat ]]
+then
+        echo "valid"
+else
+        echo "Invalid"
+fi
